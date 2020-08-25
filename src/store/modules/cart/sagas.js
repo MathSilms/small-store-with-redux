f62186287042943895fef2e25b87efa1b7eeabeb
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-08-25 15:20:45 -0300

    add reactotron-redux-saga

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,5 +1,4 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects'
-
+import { call, put, all, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
 import { addToCartSuccess }  from './actions'
 
